d3951ae4d0b90c949a8bd63584153844873127a5
Prince Asiedu <princekofasiedu@gmail.com>
2026-06-15 19:09:43 +0000

    refactor: removed unused modules and variables

--- payments/serializers.py
+++ payments/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from .models import Transaction
 
 class TransactionSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Transaction
         fields = ("amount", "phone_number", "full_name", "currency", "status")
